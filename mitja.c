604d49b0324b259688c9f49bc6669fde1dd3aa2a
Luciaamo <amorgonzalezlucia@fpllefia.com>
2024-10-01 16:43:56 +0200

    prgit-amor25 v2

--- mitja.c
+++ mitja.c
@@ -13,7 +13,7 @@ int main() {
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("amor25 Vols continuar (s/n): ");
-		scanf(" %c",&consulta); 
+		scanf(" %s",&consulta); 
 	}
 	mitja=suma/numero_repeticions;
 	printf("amor25 La mitja és: %.2f\n",mitja);	
